71c87efc9f2c4b0f386ee472c3ed9c0a2ceaad32
Niels Vandekeybus <progster@gmail.com>
2018-10-19 09:52:02 +0200

    Revert "remove rdfa for now"
    This reverts commit 2074d6f94638394ab0bf409f08a0f4d930718021.

--- addon/models/aan-te-stellen-mandataris.js
+++ addon/models/aan-te-stellen-mandataris.js
@@ -10,6 +10,14 @@ export default EmberObject.extend({
   start: null,
   einde: null,
   oudeMandaten: null,
+  rdfaBindings: { // eslint-disable-line ember/avoid-leaking-state-in-ember-objects
+    class: "http://data.vlaanderen.be/ns/mandaat#Mandataris",
+    rangorde: "http://data.vlaanderen.be/ns/mandaat#rangorde",
+    start: "http://data.vlaanderen.be/ns/mandaat#start",
+    einde: "http://data.vlaanderen.be/ns/mandaat#einde",
+    mandaat: "http://www.w3.org/ns/org#holds",
+    persoon: "http://data.vlaanderen.be/ns/mandaat#isBestuurlijkeAliasVan"
+  },
   init() {
     this._super(...arguments);
     this.set('oudeMandaten', A());

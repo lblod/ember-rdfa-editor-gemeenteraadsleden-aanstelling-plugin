f65ff3124e1fbe8e2eb83e5d52a4839618c5d988
Niels Vandekeybus <progster@gmail.com>
2018-11-27 11:09:31 +0100

    haal oude mandaten op bij bewerken

--- addon/components/mandataris-aanstelling-tabel/edit-record.js
+++ addon/components/mandataris-aanstelling-tabel/edit-record.js
@@ -2,9 +2,28 @@ import Component from '@ember/component';
 import layout from '../../templates/components/mandataris-aanstelling-tabel/edit-record';
 import AanTeStellenMandataris from '../../models/aan-te-stellen-mandataris';
 import { sort, alias } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
+import { isEmpty } from '@ember/utils';
+import { task } from 'ember-concurrency';
 
 export default Component.extend({
   layout,
+  store: service(),
+  didReceiveAttrs() {
+    this._super(...arguments);
+    if (this.record && isEmpty(this.record.oudeMandaten)) {
+      this.fetchOudeMandaten.perform();
+    }
+  },
+  fetchOudeMandaten: task( function *() {
+    const mandaten = yield this.store.query('mandataris', {
+      filter: {
+        'is-bestuurlijke-alias-van': {':uri:': this.record.persoon.uri }
+      },
+      include: 'bekleedt.bestuursfunctie'
+    });
+    this.record.set('oudeMandaten', mandaten.map((m) => AanTeStellenMandataris.create({ uri: m.uri, start: m.start, einde: m.einde, mandaat: m.bekleedt })));
+  }),
   oudeMandaten: alias('record.oudeMandaten'),
   mandatenSort:['start'],
   sortedMandaten: sort('oudeMandaten', 'mandatenSort'),

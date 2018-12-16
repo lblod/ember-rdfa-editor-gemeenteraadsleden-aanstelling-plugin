256e9a1c560b2706322cc253b86d2f4061d5af22
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-12-16 16:12:50 +0100

    extracted artikel 5 to separate component

---
+++ addon/components/mandataris-aanstelling-tabel/rangorde/output-row.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import layout from '../../../templates/components/mandataris-aanstelling-tabel/rangorde/output-row';
+
+export default Component.extend({
+  layout
+});

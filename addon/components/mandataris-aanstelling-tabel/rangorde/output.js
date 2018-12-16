143fb6166f03b90d08a31394268b5c4aa41501ba
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-12-16 16:21:38 +0100

    output by rangorde

--- addon/components/mandataris-aanstelling-tabel/rangorde/output.js
+++ addon/components/mandataris-aanstelling-tabel/rangorde/output.js
@@ -1,6 +1,12 @@
 import Component from '@ember/component';
 import layout from '../../../templates/components/mandataris-aanstelling-tabel/rangorde/output';
+import { sort } from '@ember/object/computed';
 
 export default Component.extend({
-  layout
+  layout,
+  sortedMandatarissen: sort('zetelendeMandatarissen', 'sort'),
+  init() {
+    this._super(...arguments);
+    this.set('sort', ['rangorde']);
+  }
 });

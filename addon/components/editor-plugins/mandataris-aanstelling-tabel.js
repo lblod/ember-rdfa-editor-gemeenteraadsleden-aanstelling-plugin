bd82e50f590157fb55acc33c215414a3237cbc38
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2018-12-20 16:56:20 +0100

    revert changes for sorting on rangorde
    - also disable the removal of verkozene

--- addon/components/editor-plugins/mandataris-aanstelling-tabel.js
+++ addon/components/editor-plugins/mandataris-aanstelling-tabel.js
@@ -2,17 +2,10 @@ import Component from '@ember/component';
 import layout from '../../templates/components/editor-plugins/mandataris-aanstelling-tabel';
 import { inject as service } from '@ember/service';
 import { isEmpty } from '@ember/utils';
-import { computed } from '@ember/object';
 
 export default Component.extend({
   layout,
   store: service(),
-
-  verkozenen: computed('mandatarissen', 'mandatarissen.[]', 'mandatarissen.@each.rangorde', function(){
-    this.renumberVerkozenen();
-    return this.mandatarissen.sortBy('rangorde');
-  }),
-
   didReceiveAttrs() {
     this.set('record', null);
     if (!isEmpty(this.mandatarissen)) {
@@ -27,31 +20,31 @@ export default Component.extend({
           mandataris.set('rangorde', rangorde);
         }
       });
+      this.set('verkozenen', this.mandatarissen.sortBy('rangorde'));
     }
   },
   renumberVerkozenen() {
     var orde = 1;
-    for(var verkozene of this.mandatarissen) {
+    for(var verkozene of this.verkozenen) {
       verkozene.set('rangorde', orde++);
     }
   },
   actions: {
     up(verkozene) {
-      const index = this.mandatarissen.indexOf(verkozene);
+      const index = this.verkozenen.indexOf(verkozene);
       if (index > 0) {
-        this.mandatarissen.removeObject(verkozene);
-        this.mandatarissen.insertAt(index -1, verkozene);
+        this.verkozenen.removeObject(verkozene);
+        this.verkozenen.insertAt(index -1, verkozene);
+        this.renumberVerkozenen();
       }
     },
     down(verkozene) {
-      const index = this.mandatarissen.indexOf(verkozene);
-      if (index < this.mandatarissen.length) {
-        this.mandatarissen.removeObject(verkozene);
-        this.mandatarissen.insertAt(index + 1, verkozene);
+      const index = this.verkozenen.indexOf(verkozene);
+      if (index < this.verkozenen.length) {
+        this.verkozenen.removeObject(verkozene);
+        this.verkozenen.insertAt(index + 1, verkozene);
+        this.renumberVerkozenen();
       }
-    },
-    remove(verkozene) {
-      this.mandatarissen.removeObject(verkozene);
     },
     setRecord(verkozene) {
       this.setRecord(verkozene);
